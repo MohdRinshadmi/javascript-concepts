bbfb8b7573695bf9a8d9d3b566c8f0b2ee37b904
Rinshad <rinshad@teamioss.in>
2025-06-24 17:55:08 +0530

    currying modified

--- Currying/index.js
+++ Currying/index.js
@@ -1,3 +1,20 @@
+// 1. What is currying in javascript?
+//   >>  Currying is used in javascript to break down complex function calls into smaller, more manageable steps. It transforms a function with multiple arguments into a series of functions, each taking a single argument.
+
+// - [ ]  It converts a function with multiple parameters into a sequence of functions.
+// - [ ]  Each function takes a single argument and returns another function until all arguments are received.
+// - [ ]  Helps in functional programming by enabling function reusability and composition.
+
+
+// 2. When to use currying function?
+//   >>  In javascript, currying function is used in the following cases
+
+// - [ ]  Partial Application: In the partial application we set some arguments in advance in the function and call it later with the remaining arguments.
+// - [ ]  High-Order Functions: When one function takes the other function as arguments (eg: map, filter , reduce) in that case we can use the currying function to manage the arguments more effectively.
+// - [ ]  Functional Programming: Where functions are treated as important and focus is on not changing data and combining functions in those cases currying works perfectly.
+
+
+
 let multiply = function(x, y){
     console.log(x * y);
 }
---
+++ JavaScript/JavaScriptConcept.js
@@ -0,0 +1,9 @@
+// Browser (as a part of websites)
+// JavaScript code can be included in any website. The code then executes inside the browser (i.e on the machine of the website visitor).
+
+
+// on any computer (eg: server side code)
+// Thanks to the Node.js or Deno. JavaScript code can executed outside of the browser too, the code then executes directly on the machine.
+
+// on mobile devices (eg: via embedded websites)
+// With extra technologies like React Native, you can build mobile apps based on JavaScript. The code then executes on mobile device.
